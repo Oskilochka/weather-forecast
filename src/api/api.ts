76e17614dbb69b88e754ba521923cf4cf2c709ae
Oskilochka <yulia.osk14@gmail.com>
2021-03-02 16:30:25 +0200

    hide api key and add to api via process

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-/*const API_KEY = "82f8ae70e0e158de1f8d15a9b6987f50"*/
+const API_KEY = process.env.REACT_APP_API_KEY
 
 const instance = axios.create({
     baseURL: "https://api.openweathermap.org/data/2.5/forecast"
@@ -9,7 +9,7 @@ const instance = axios.create({
 export const weatherAPI = {
     async getWeatherData(cityName: string, units: string) {
         const res = await instance.get(
-            `?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=${units}`
+            `?q=${cityName}&appid=${API_KEY}&units=${units}`
         );
         return res;
     },
